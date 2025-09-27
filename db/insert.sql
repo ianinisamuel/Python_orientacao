b92d9c07f55c21195647e749ba6e12b0b77a7c99
Samuel Ianini <ianinisamuel@gmail.com>
2025-09-26 22:14:44 -0300

    Atualização 2023-06-14

---
+++ db/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO pedidos (mesa, status) VALUES
+(1, 'Em preparo'),
+(2, 'Servido'),
+(3, 'Cancelado');
+INSERT INTO lanches (name, description, price) VALUES
+('X-Burger', 'Hambúrguer com queijo, alface, tomate e maionese', 15.00),
+('X-Salada', 'Hambúrguer com queijo, alface, tomate, cebola e maionese', 18.00),
+('X-Bacon', 'Hambúrguer com queijo, bacon, alface, tomate e maionese', 20.00);
